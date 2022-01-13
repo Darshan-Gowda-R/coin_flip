0c917ff709d26d5db85d7eb55efb502511d3c30e
darshan <darshan.gowda.r.100@gmail.com>
2022-01-13 14:11:35 +0530

    added the loop to count the flips

--- flip_coin.sh
+++ flip_coin.sh
@@ -1,12 +1,20 @@
 #!/bin/bash -x
 
-val=$((RANDOM%2))
 
-if [ $val == '0' ]
-then
-echo HEAD
-else
-echo TAIL
 
-fi
-echo $val
+
+tail_score=0
+head_score=0
+
+for ((i=0;i<10;i++))
+do
+	val=$((RANDOM%2))
+	if [ $val == '0' ]
+	then
+		head_score=$((head_score+1))
+	else
+		tail_score=$((tail_score+1))
+
+	fi
+done
+echo $tail_score  $head_score
